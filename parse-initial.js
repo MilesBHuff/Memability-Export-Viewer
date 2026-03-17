fcc5ace608560c21abd593e0a43f642bb375df0a
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2026-03-16 22:31:59 -0400

    As part of cleaning the data, collapse the hierarchy.

--- parse-initial.js
+++ parse-initial.js
@@ -1,28 +1,35 @@
 async function main() {
-    let data;
 
     // Load JSON file
-    const dataPath = 'Memability.json'; // This is an export from Google Tasks of "tasks" created by the app Memability/Memz.co in the process of backing up my notes.
+    let rawData;
+    const rawDataPath = 'Memability.json'; // This is an export from Google Tasks of "tasks" created by the app Memability/Memz.co in the process of backing up my notes.
     try {
-        data = (await (await fetch(dataPath)).json()).items[0].items; // The only index is the Memability data.
+        rawData = (await (await fetch(rawDataPath)).json()).items[0].items; // The only index is the Memability data.
     } catch(error) {
-        console.error(`"${dataPath}" failed to load!`);
+        console.error(`"${rawDataPath}" failed to load!`);
     }
 
     // Clean the data
-    let cleanData = [];
-    for(const datum of data) {
-        cleanData.push({
-            timestamp: '2014-12-31T23:59:59.999999Z', // The `updated` field is, unfortunately, the same for all of these notes, and wildly far-off of their creation dates; so I'm giving everything a reasonably representative timestamp.
-            title: datum.title ?? '',
-            text: !datum.notes ? '' : datum.notes.replaceAll('\\\n', '\\n').replaceAll('\\\t', '\\t'), // Escaped sequences need to be normalized before display.
-        });
-    }
-    data = undefined;
+    let cleanedData = [];
+    const cleanData = data => {
+        for(const datum of data) {
+            cleanedData.push({
+                id: datum.id,
+                id_parent: datum.parent,
+                timestamp: '2014-12-31T23:59:59.999999Z', // The `updated` field is, unfortunately, the same for all of these notes, and wildly far-off of their creation dates; so I'm giving everything a reasonably representative timestamp.
+                title: datum.title ?? '',
+                text: !datum.notes ? '' : datum.notes.replaceAll('\\\n', '\\n').replaceAll('\\\t', '\\t'), // Escaped sequences need to be normalized before display.
+            });
+            if(datum.items?.length ?? 0 > 0) {
+                cleanData(datum.items)
+            }
+        }
+    };
+    cleanData(rawData)
 
     // Display the data
-    console.log(cleanData);
-    for(const datum of cleanData) {
+    console.log(cleanedData);
+    for(const datum of cleanedData) {
         const output = document.getElementById('output');
 
         const container = document.createElement('div');
