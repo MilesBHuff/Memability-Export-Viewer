3ddd7cd53a6890d15b560173a223dccadfd7ff2b
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2026-03-17 07:57:42 -0400

    Consistencize case.

--- dump-db.sh
+++ dump-db.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 sqlite3 notes-sqlite.db -json "
 SELECT
-  n.ID as id,
-  n.PARENT_ID as parent,
-  strftime('%Y-%m-%dT%H:%M:%S', n.CREATED / 1000, 'unixepoch') || '.000000Z' as created,
-  strftime('%Y-%m-%dT%H:%M:%S', n.UPDATED / 1000, 'unixepoch') || '.000000Z' as updated,
-  CASE WHEN n.REMOVED = 0 THEN NULL ELSE strftime('%Y-%m-%dT%H:%M:%S', n.REMOVED / 1000, 'unixepoch') || '.000000Z' END as removed,
-  n.TITLE as title,
-  t.TEXT_FULL as notes
-FROM NOTE n
-LEFT JOIN NOTE_TEXT t ON n.ID = t.NOTE_ID
-ORDER BY n.PARENT_ID, n.REMOVED, n.UPDATED DESC, n.CREATED DESC, n.ID;
+  N.ID as id,
+  N.PARENT_ID as parent,
+  strftime('%Y-%m-%dT%H:%M:%S', N.CREATED / 1000, 'unixepoch') || '.000000Z' as created,
+  strftime('%Y-%m-%dT%H:%M:%S', N.UPDATED / 1000, 'unixepoch') || '.000000Z' as updated,
+  CASE WHEN N.REMOVED = 0 THEN NULL ELSE strftime('%Y-%m-%dT%H:%M:%S', N.REMOVED / 1000, 'unixepoch') || '.000000Z' END as removed,
+  N.TITLE as title,
+  T.TEXT_FULL as notes
+FROM NOTE N
+LEFT JOIN NOTE_TEXT T ON N.ID = T.NOTE_ID
+ORDER BY N.PARENT_ID, N.REMOVED, N.UPDATED DESC, N.CREATED DESC, N.ID;
 " | jq '{items: [{title: "Memability", items: .}]}' > Memability.json
