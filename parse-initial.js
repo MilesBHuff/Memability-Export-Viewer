264d17d7dab185ce9742b2c22755b01639be4278
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2026-03-16 21:28:49 -0400

    The script now loads the JSON file.

--- parse-initial.js
+++ parse-initial.js
@@ -1,3 +1,10 @@
-function main() {
-    document.getElementById('output').innerHTML = 'TODO';
+async function main() {
+    const dataPath = 'Memability.json';
+    try {
+        const data = await (await fetch(dataPath)).json();
+        console.log(data);
+        document.getElementById('output').innerHTML = data;
+    } catch(error) {
+        console.error(`"${dataPath}" failed to load!`);
+    }
 }
